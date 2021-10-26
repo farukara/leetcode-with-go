4eaa7ca940f0a918f4c862d992be41dde62ac165
farukara <69348522+farukara@users.noreply.github.com>
2021-10-26 22:14:02 +0300

    modded 0007

--- files/0007. Reverse Integer.go
+++ files/0007. Reverse Integer.go
@@ -3,19 +3,29 @@ package main
 
 import (
     "fmt"
+    "strconv"
+    "math"
 )
 
 func reverse(x int32) int32 {
-   if x < 0 {
-       var byty []byte
-       byty = byte(x)
-       fmt.Println(byty)
+    var remainder int32
+    var result string
+   if x > 0 {
+       for {
+           if x>9 {
+               remainder = x%10
+               x = math.Floor(x/10)
+           } else {
+               result = append(result)
+
+           }
+       }
    } 
-   return byte
+   return 32
 }
 
 func main() {
-    inputs := []int32{123, -123, 120}
+    inputs := []int32{123, -123, 120, 121, 122}
     for _, input := range inputs {
         reverse(input)
     }
