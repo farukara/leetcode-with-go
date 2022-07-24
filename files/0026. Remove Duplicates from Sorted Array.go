08a53a77445faacda4c875b35a989db5eb520dc3
farukara <69348522+farukara@users.noreply.github.com>
2022-07-24 18:50:59 +0300

    added 0026

--- files/0026. Remove Duplicates from Sorted Array.go
+++ files/0026. Remove Duplicates from Sorted Array.go
@@ -6,13 +6,36 @@ import (
 )
 
 func removeDuplicates(nums []int) int {
-    
+    if len(nums) == 1 {return 1}
+    s := 0;
+    f := 1;
+    for f<len(nums) {
+        if nums[s] == nums[f] {
+            f++
+            if f>=len(nums) {break}
+            for nums[s] == nums[f] {
+                f++
+                if f>=len(nums) {break}
+            }
+            if f>=len(nums) {break}
+            s++
+            nums[s] = nums[f]
+            continue
+        } else {
+            s++
+            f++
+        }
+    }
+    return s+1
 }
 
 func main () {
     nums := []int{1,1,2}  // k=2 {1,2,_}
     fmt.Println(removeDuplicates(nums))
 
-    nums := []int{0,0,1,1,1,2,2,3,3,4}  // k=5 {0,1,2,3,4,_,_,_,_,_}
+    nums = []int{0,0,1,1,1,2,2,3,3,4}  // k=5 {0,1,2,3,4,_,_,_,_,_}
+    fmt.Println(removeDuplicates(nums))
+
+    nums = []int{1,1,1}  // k=1 {1,_,_}
     fmt.Println(removeDuplicates(nums))
 }
